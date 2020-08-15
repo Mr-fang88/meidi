769dddeb89f1a46cfdf630637d16f78f096db26d
Mr-Fang <1323621305@qq.com>
2020-08-15 16:28:11 +0800

    更新了routes/users.js

--- meidi/myapp/routes/users.js
+++ meidi/myapp/routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var { db } = require('../db/db');
+
+router.all('*',function(req,res,next){
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Methos','*');
+  next();
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('user ing');
 });
 
 module.exports = router;
